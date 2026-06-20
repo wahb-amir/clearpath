906327e156d3abfc0f045f4603360a76cddc9525
Wahb Amir <wahbamir2010@gmail.com>
2026-06-20 21:36:40 +0500

    add maxAge to cookies

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -32,6 +32,7 @@ const authCookieOptions = {
   secure: isProd,
   sameSite: isProd ? ("none" as const) : ("lax" as const),
   path: "/",
+  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 };
 
 const setAuthCookies = (
